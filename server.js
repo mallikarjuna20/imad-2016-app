5e98a936a5e34a81f5f58b5b88458c4bde259241
mallikarjuna20 <rammallikarjunarao@gmail.com>
2016-11-02 11:41:29 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, '/', 'secondFile.html'));
+  res.sendFile(path.join(__dirname, 'ui', 'secondFile.html'));
 });
 
 app.get('/ui/style.css', function (req, res) {
